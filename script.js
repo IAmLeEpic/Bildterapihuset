0cd55e75eebc89e9c972def7957217cc08ba7c37
IAmLeEpic <te15.jonathan.sjogren@umea.nti.se>
2018-02-13 15:10:56 +0100

    Gjorde nästan klart startsidan
    Lagt till bild och navbar, gjord om designen på startsidan och börjat med javascript för navbar smoothscroll och annan funktion.

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	// #links smooth scroll
+	$('a').click(function() {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1500);
+		return false;
+	});
+	// End of smooth scroll
+
+	console.log($('html').scrollTop());
+
+});
+
+
