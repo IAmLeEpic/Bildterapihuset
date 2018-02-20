c33dd5a4cd9548b6ced73034aa1c1482b8dcf588
IAmLeEpic <te15.jonathan.sjogren@umea.nti.se>
2018-02-20 15:11:19 +0100

    Navbar ändringar, nu fixed när man scrollar ner
    Navbar blir nu fixed då man skrollar ner på sidan. Lagt till två till "sections" och ska börja med deras layout sen.

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ $(document).ready(function() {
 
 	console.log($('html').scrollTop());
 
+	window.onscroll = changePos;
+
+	function changePos() {
+		var header = document.getElementById("topnav");
+		var sectionMargin = document.getElementById("section2");
+		if ($('html').scrollTop() > 800) {
+			console.log($('html').scrollTop());
+			header.style.position = "fixed";
+			header.style.top = "0px";
+			sectionMargin.style.paddingTop = "58px";
+
+		}else{
+			header.style.position = "relative";
+			header.style.top = "0";
+			sectionMargin.style.paddingTop = "10px";
+		}
+	}
+
 });
 
 
